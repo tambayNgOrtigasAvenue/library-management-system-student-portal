a374c6e438c1d06eef66b614bf350276b8ef3aa8
tambayNgOrtigasAvenue <markkengiealdabon@gmail.com>
2025-04-27 14:14:44 +0800

    migrations for student logs

--- database/migrations/2025_04_26_152450_create_tbl_logs_table.php
+++ database/migrations/2025_04_26_152450_create_tbl_logs_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('tbl_logs', function (Blueprint $table) {
             $table->id();
+            $table->string('student_id', 7);
+            $table->string('last_name', 55);
+            $table->string('first_name', 55);
+            $table->string('middle_name', 55)->nullable();
+            $table->string('program', 255);
+            $table->string('section', 7);
+            $table->date('entry_date');
+            $table->time('entry_time');
             $table->timestamps();
         });
     }
