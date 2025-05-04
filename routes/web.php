5c5152fc6ce468e64a0da25aded6760c04b0a458
tambayNgOrtigasAvenue <markkengiealdabon@gmail.com>
2025-05-05 00:11:54 +0800

    Fix routing for homepage, route name for post method of student form, and remove unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LogController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/entryform', function(){
-    return view('entry_form');
-});
-
-Route::get('/form', function(){
-    return view('form');
-});
-
-// Added route because when I try to access other routes, the error is looking for this routes
-
-Route::get('/home', function () {
     return view('homepage');
 })->name('home');
 
-Route::get('/appointment/form', function () {
-    return view('appointment_form');
-})->name('appointment.form');
+Route::get('/form', function(){
+    return view('form');
+})->name('form');
+
+Route::post('/log-student', [LogController::class, 'logStudent'])->name('log.student');
+
+// Added route because when I try to access other routes, the error is looking for this routes
 
 Route::post('/library/entry/store', function () {
     // Handle the form submission logic here
@@ -42,17 +33,12 @@ use Illuminate\Support\Facades\DB;
 Route::get('/test-oracle', function() {
     try {
         DB::connection()->getPdo();
-        return "Connected to Oracle 23c Free!";
+        return "Connected to Oracle 23ai Free!";
     } catch (\Exception $e) {
         return "Error: " . $e->getMessage();
     }
 });
 
 Route::get('/success-message', function(){
-    return view('succesfull');
+    return view('successfull');
 });
-
-use App\Http\Controllers\LogController;
-
-Route::get('/attendance', [LogController::class, 'create'])->name('logs.create');
-Route::post('/attendance', [LogController::class, 'store'])->name('logs.store');
